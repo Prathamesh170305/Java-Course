62b162c558b012ed939d28c2c8a8059b8e2bab63
Prathamesh170305 <wankhadeprathamesh17@gmail.com>
2026-04-17 13:58:43 +0530

    done with the loops

---
+++ Lecture-13_Loops/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+        // while
+        int i=0;
+        while(i<10){
+            System.out.println("yoo");
+            i++;
+        }
+        //do while
+        do{
+            i++;
+            System.out.println(("hhhh"));
+        }while(i<20);
+        //for loop
+        for(int j=0;j<10;j++){
+            System.out.println(("doen"));
+        }
+    }
+}
