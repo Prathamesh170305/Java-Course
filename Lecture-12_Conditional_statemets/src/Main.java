f7a37a7b89d94c61abb2698914dab28c6b6e6cb2
Prathamesh170305 <wankhadeprathamesh17@gmail.com>
2026-04-17 13:50:44 +0530

    conditional statements

---
+++ Lecture-12_Conditional_statemets/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args){
+        int a=1;
+        int b=2;
+        boolean c=a!=b;
+        System.out.println(c);
+
+        int ramMarks=35;
+        int ramAge=19;
+
+        System.out.println(ramMarks>=33 && ramAge>=18);
+
+        int age=18;
+        if(age>=18 ) { //condition
+            //then
+            System.out.println("You are an adult!");
+        }else{
+            //else block
+        }
+
+        
+    }
+}
